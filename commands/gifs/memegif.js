ad48e4e1b3c0d763c968d063968c1ff04e705686
Astrogamer54 <78519393+Astrogamer54@users.noreply.github.com>
2022-01-09 11:28:38 -0800

    e

--- commands/gifs/memegif.js
+++ commands/gifs/memegif.js
@@ -7,9 +7,9 @@ if (!tenorAPI) return;
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('animegif')
-    .setDescription('Responds with a random anime gif'),
+    .setDescription('Responds with a random meme gif'),
   execute(interaction) {
-    fetch(`https://g.tenor.com/v1/random?key=${tenorAPI}&q=anime&limit=50`)
+    fetch(`https://g.tenor.com/v1/random?key=${tenorAPI}&q=memes&limit=50`)
       .then(res => res.json())
       .then(json =>
         interaction.reply(json.results[Math.floor(Math.random() * 49)].url)
