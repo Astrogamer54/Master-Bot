8c4f0a2806ab2fcc8b73380732e4edc2bfeeb0fa
Astrogamer54 <78519393+Astrogamer54@users.noreply.github.com>
2022-01-12 16:03:52 -0800

    Update dm.js

--- commands/other/dm.js
+++ commands/other/dm.js
@@ -19,23 +19,27 @@ module.exports = {
     ),
 
   execute(interaction) {
-    const user = interaction.options.get('user').user;
-    const texttosend = interaction.options.get('message').value;
-    console.log(texttosend)
-    console.log(user)
-    console.log(texttosend)
     try{
-      user.send(texttosend);
+      const user = interaction.options.get('user').user;
+      const texttosend = interaction.options.get('message').value;
+      console.log(texttosend)
+      console.log(user)
+      console.log(texttosend)
+      try{
+        user.send(texttosend);
+      }catch(err){
+        return interaction.reply(err);
+      }
+      const embed = new MessageEmbed()
+        .setTitle(user.username)
+        .setDescription(`Dm'ed ${user.username}: ${texttosend}`)
+        .setImage(user.displayAvatarURL({ dynamic: true }))
+        .setColor('0x00ae86');
+      
+
+      return interaction.reply({ embeds: [embed] });
     }catch(err){
       return interaction.reply(err);
     }
-    const embed = new MessageEmbed()
-      .setTitle(user.username)
-      .setDescription(`Dm'ed ${user.username}: ${texttosend}`)
-      .setImage(user.displayAvatarURL({ dynamic: true }))
-      .setColor('0x00ae86');
-    
-
-    return interaction.reply({ embeds: [embed] });
   }
 };
